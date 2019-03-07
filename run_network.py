0dfb24e32b76068deec319b019f583f6354c2708
PetrKudy <44304564+PetrKudy@users.noreply.github.com>
2019-03-07 15:07:34 +0100

    Add files via upload

---
+++ run_network.py
@@ -0,0 +1,9 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zebrabase.settings")
+
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application 
+if __name__ == "__main__":
+	application = get_wsgi_application()
+
+	call_command('runserver',  '0.0.0.0:8000')
